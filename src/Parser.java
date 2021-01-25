7e6cc40a79c8bef57a3696b2a3b1e0c4bdde54fd
DvynilHub <belonogov220@gmail.com>
2021-01-26 00:34:01 +0800

    First version

--- src/Parser.java
+++ src/Parser.java
@@ -1,2 +1,16 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class Parser {
+    Document doc;
+
+    {
+        try {
+            doc = Jsoup.connect("https://hh.ru/").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
